9fa044313befc9abb396d83469074fdb1b429bec
Ousman Jaguraga <ousjaguraga19@gmail.com>
2022-01-28 16:10:23 +0000

    Initial Commit

---
+++ Shared/Allah_Toxo_SiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Allah_Toxo_SiruApp.swift
+//  Shared
+//
+//  Created by Ovenger on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Allah_Toxo_SiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
