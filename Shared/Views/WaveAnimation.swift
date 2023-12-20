dc936246d5f54311b452c2950911f3c55c5a9306
ousjaguraga <ousjaguraga19@gmail.com>
2023-12-20 23:04:39 +0000

    Did refactoring and removed play button from playall screen

---
+++ Shared/Views/WaveAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  WaveAnimation.swift
+//  Allah Toxo Siru (iOS)
+//
+//  Created by Ovenger on 20/12/2023.
+//
+
+import SwiftUI
+
+struct WaveAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaveAnimation()
+}
